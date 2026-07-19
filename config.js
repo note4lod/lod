022b38c693f51633f8bc5ed89f26393de93efcc6
note4lod <note4lod@gmail.com>
2026-07-19 14:19:09 +0300

    Enable all levels in MASTER build

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.EP_CONFIG = Object.freeze({
-  MASTER_MODE: false,
-  VERSION: "1.5.0",
-  CACHE_NAME: "ep-maneuver-v1.5-normal"
+  MASTER_MODE: true,
+  VERSION: "1.5.0-MASTER",
+  CACHE_NAME: "ep-maneuver-v1.5-master"
 });
