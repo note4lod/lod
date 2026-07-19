18e93860d06933b76765725f84767ae788036ab6
note4lod <note4lod@gmail.com>
2026-07-19 16:39:06 +0300

    Stabilize mobile rendering at 30 FPS

--- config.js
+++ config.js
@@ -3,3 +3,7 @@ window.EP_CONFIG = Object.freeze({
   VERSION: "1.7.0-MASTER",
   CACHE_NAME: "ep-maneuver-v1.7-master"
 });
+
+// Ağır ECDIS konturlarında iPhone tarayıcısının kare atlamasını ve kıyı titremesini azaltır.
+window.requestAnimationFrame = (callback) => window.setTimeout(() => callback(performance.now()), 33);
+window.cancelAnimationFrame = (handle) => window.clearTimeout(handle);
