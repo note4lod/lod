9159fa8ddae0ea21a840420215ead8ed698e5946
note4lod <note4lod@gmail.com>
2026-07-19 16:35:25 +0300

    Bump MASTER version to v1.7

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.EP_CONFIG = Object.freeze({
   MASTER_MODE: true,
-  VERSION: "1.6.0-MASTER",
-  CACHE_NAME: "ep-maneuver-v1.6-master"
+  VERSION: "1.7.0-MASTER",
+  CACHE_NAME: "ep-maneuver-v1.7-master"
 });
